0d25fbba08d8ea14310ef44fb88cca713e4f2587
lvalencia <luis.valencia@basisworldwide.com>
2014-09-22 20:15:41 -0700

    Added COmfy Mexican Sofa Staged the Views Set Up some bascis

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 MostlyKosher::Application.routes.draw do
   root "client#landing"
+  comfy_route :cms_admin, :path => '/admin'
+
+  # Make sure this routeset is defined last
+  comfy_route :cms, :path => '/', :sitemap => false
 end
