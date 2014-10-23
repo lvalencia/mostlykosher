47f24bbf3a5ef4fa05e0699eaee64077846f1a64
lvalencia <luis.valencia@basisworldwide.com>
2014-10-22 23:07:27 -0700

    Added Gallery Section

--- app/assets/javascripts/client.js
+++ app/assets/javascripts/client.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
    $("abbr.timeago").timeago();
    $('#map_canvas').gmap();
+   $("#gallery-preview").owlCarousel({
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+      autoHeight: true,
+      items: 3,
+  });
 });
