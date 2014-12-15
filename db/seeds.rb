aad2721697f56a1247c8870921c97634159341b2
lvalencia <luis.valencia@basisworldwide.com>
2014-12-15 13:57:45 -0800

    Added TempFile for Review Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Post.create({ feed:"twitter", title:"Oct, 24", posted_date:DateTime.parse("Oct 24"), created_at:DateTime.parse("Oct 24"), updated_at:DateTime.parse("Oct 24"), content:"WHAT??? You say there are only EIGHT reserved seating tickets left for the Mostly Kosher CD release party???... http://fb.me/3KR16AUE2" })
-Post.create({ feed:"twitter", title:"Oct, 25", posted_date:DateTime.parse("Oct 25"), created_at:DateTime.parse("Oct 25"), updated_at:DateTime.parse("Oct 25"), content:"Our CDs have arrived! Pick yours up on November 1st at Pico Union Project on Nov. 1st!... http://fb.me/27kUCJpbE " })
-Post.create({ feed:"facebook", title:"Oct, 25", posted_date:DateTime.parse("Oct 25"), created_at:DateTime.parse("Oct 25"), updated_at:DateTime.parse("Oct 25"), content:"1. Our CDs have arrived! Pick yours up on November 1st at Pico Union Project on Nov. 1st! www.mostlykosherpicounion.eventbrite.com<br />2. There are only 3 MORE TICKETS as of this posting for reserved seating!<br/>3. Have ya seen our music video? https://www.youtube.com/watch?v=tsYZTFfYD6U Feel free to LIKE and SHARE!" })
-Review.create({ created_at:DateTime.parse("Aug 2"), updated_at:DateTime.now, posted_date:DateTime.parse("Aug 2"), entity:"Valley Scene Magazine", quote:"<a href=\"http://mostlykosher.com/MK.valleyScene.pdf\">\"MOSTLY KOSHER IS ALL GOOD\"</a>" })
+Review.create({
+   quote:"The two bands [Mostly Kosher and the Klezmatics] share a love for preserving traditional Yiddish music and cultural traditions — sometimes with the addition of a contemporary spin — and both have enjoyed plenty of critical and audience acclaim. Still, Mostly Kosher and the Klezmatics can, to some extent, be viewed as the new klezmer kid on the block and the genre’s lion, respectively.",
+   entity:"Evan Henderson, Los Angeles Jewish Journal Magazine"
+})
+Review.create({
+   quote:"I had very little involvement in last night's sold out Invisible Hour at the Pico Union, but it was a highlight event of the year for me. I loved the music, the crowd and the energy - but most of all I loved watching really great people do their thing really well and I love what they were doing. All honor to Mostly Kosher for a superb evening!",
+   entity:"Craig Taubman, Pico Union Project"
+})
+Review.create({
+   quote:"Mostly Kosher shined throughout their concert at BCC, making use of their exquisite musicianship, contagious energy and poignant light-hearted humour -all of which were great ways to introduce the attendees to the sonic and spiritual experience that Klezmer music is about!",
+   entity:"Cantor Juval Porat, Temple Beth Chayim Chadashim"
+})
+Review.create({
+   quote:"Mostly Kosher perfectly complimented our Jews in LA exhibit. The traditional music and songs were beautifully executed and performed. The comedy sketches disbursed among the musical performances were hilarious. Through comedy very real and moving moments representing Jewish culture were respectively brought to life.",
+   entity:"Robyn Hetrick, Director of Programs and Public Events, Autry Museum"
+})
+Review.create({
+   quote:"Your group’s enthusiasm, wit, talent and high energy created a one-of-a-kind experience for our visitors.” – Michael Fritzen, Head of Family Programs, Skirball Cultural Center",
+   entity:"Michael Fritzen, Head of Family Programs, Skirball Cultural Center"
+})
+
