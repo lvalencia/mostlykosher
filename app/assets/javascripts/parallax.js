fe69df55e7c70edea754495b90e1cf3d8f203e73
lvalencia <luis.valencia@basisworldwide.com>
2015-01-13 00:10:12 -0800

    Parallax Scrolling Works Now

--- app/assets/javascripts/parallax.js
+++ app/assets/javascripts/parallax.js
@@ -1,29 +1,40 @@
-/* Dynamic Resize  */
-$(document).ready(function () {
-});
-$(document).ready(function () {
+ready = function () {
    $window = $(window);
-
-   $('[data-parallax="back"]').each(function () {
+   $('[data-parallax="back"]').css('height', $(window).height()); 
+   // For each element that has a data-type attribute
+   $('[data-type="background"]').each(function () {
       var $self = $(this);
+      // Store function
       function imageParallax() {
          var offsetCoords = $self.offset();
          var topOffset = offsetCoords.top;
+         // If this section is in view
          if (($window.scrollTop() + $window.height()) > (topOffset) && ((
             topOffset + $self.height()) > $window.scrollTop())) {
+            // Scroll speed and Y position
             var yPos = -(($window.scrollTop() - topOffset) / 5);
-            $self.find('[data-parallax=wrapper]').first().css({
-                'left':0,
-                'top':yPos+'px'
+            // Final background position
+            var coords = '50% ' + yPos + 'px';
+            if ($self.attr('id') == "about"){   
+                coords = '50% '+(yPos+$self.height()/4)+'px';
+            }
+            // Apply the CSS
+            $self.css({
+               backgroundPosition: coords
             });
          };
       }
-      $('div.parallax.row').scroll(function () {
+
+      // On Scroll
+      $window.scroll(function () {
          imageParallax();
       });
+      // On Resize
       $window.resize(function () {
          imageParallax();
       });
    });
+}
 
-});
+$(window).on('resize', ready);
+$(document).ready(ready);
