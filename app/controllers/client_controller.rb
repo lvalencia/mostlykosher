f5270c002d5ad798ab30a35e1ef2c680114ac279
Luis Valencia <luis.valencia@basisworldwide.com>
2016-12-06 21:50:28 -0800

    Added Rubocop for standardization Adjusted stylesheet for shows Added global navbar Updated Paritals for shows

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,27 +1,25 @@
 class ClientController < ApplicationController
+  def landing
+    @reviews = Review.latest_reviews
+    @news_posts = Post.latest_twitter_posts
+    @facebook_posts = Post.latest_facebook_posts
+    @next_event = Event.next
+    @images = Post.latests_with_images.map { |post| post.image.url(:gallery_preview) }
+    @is_ios = request_is_ios
+  end
 
-   def landing
-      @reviews = Review.latest_reviews
-      @news_posts = Post.latest_twitter_posts
-      @facebook_posts = Post.latest_facebook_posts
-      @next_event = Event.next
-      @images = Post.latests_with_images.map { |post| post.image.url(:gallery_preview) }
-      @is_ios = request_is_ios
-   end
+  def shop
+  end
 
-   def shop
-   end
+  def shows
+    @previous_shows = Event.previous_shows
+    @upcoming_shows = Event.upcoming_shows
+  end
 
-   def shows
-      @previous_shows = Event.previous_shows
-      @upcoming_shows = Event.upcoming_shows
-   end
+  private
 
-private
-
-   def request_is_ios
-      user_agent = request.env['HTTP_USER_AGENT'].to_s.downcase
-      return (user_agent.include? 'ipod' or user_agent.include? 'ipad' or user_agent.include? 'iphone')
-   end
-
+  def request_is_ios
+    user_agent = request.env['HTTP_USER_AGENT'].to_s.downcase
+    (user_agent.include? 'ipod' || user_agent.include?('ipad') || user_agent.include?('iphone'))
+  end
 end
