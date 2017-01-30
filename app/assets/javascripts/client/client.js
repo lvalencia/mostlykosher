74e5c7344dd99ea08b77f79636e5bec06cc54ef9
Luis Valencia <luis.valencia@basisworldwide.com>
2017-01-29 20:15:53 -0800

    Encapsulated client.js in a closure Removed Railway fonts Added Mobile Responsive Styling to About Section Fixed some typos in the about text Removed Card Info, we're not using it. Created new file for About styling, added to import in application

--- app/assets/javascripts/client/client.js
+++ app/assets/javascripts/client/client.js
@@ -1,35 +1,34 @@
+(function ($){
 // Bindings
-ready = function () {
-  // Time Ago Setup
-  jQuery.timeago.settings.allowFuture = true;
-  $("abbr.timeago").timeago();
-  $("#gallery-preview").owlCarousel({
-    autoPlay: 3000, //Set AutoPlay to 3 seconds
-    autoHeight: true,
-    items: 3
-  });
-  // Promote Any Promotions
-  Promotion({
-    target: '#tour-modal, #residency-modal',
-    close:  '#tour-modal .close'
-  }).show();
-  // Newsletter Signup
-  Newsletter({
-    target: 'form#newsletter'
-  }).initNewsletter();
-};
+  ready = function () {
+    // Time Ago Setup
+    $.timeago.settings.allowFuture = true;
+    $("abbr.timeago").timeago();
+    $("#gallery-preview").owlCarousel({
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+      autoHeight: true,
+      items: 3
+    });
+    // Newsletter Signup
+    Newsletter({
+      target: 'form#newsletter'
+    }).initNewsletter();
+  };
 // Navigation Scroll
-$(document).on("click", "nav ul li a[href^='#']", function(e){
-  e.preventDefault();
-  var header_offset = 40;
-  var delay = 1e3;
-  var target = $($(this).attr("href"));
-  $("html, body").animate({
-    scrollTop:target.offset().top-header_offset
-  }, delay);
-});
+  $(document).on("click", "nav ul li a[href^='#']", function(e){
+    e.preventDefault();
+    var header_offset = 40;
+    var delay = 1e3;
+    var target = $($(this).attr("href"));
+    $("html, body").animate({
+      scrollTop:target.offset().top-header_offset
+    }, delay);
+  });
 // Ready Trigger
-$(document).on('ready page:load turbolinks:load', ready);
+  $(document).on('ready page:load turbolinks:load', ready);
 // Set Google Maps Callback
-window.MostlyKosher = window.MostlyKosher || {}
-window.MostlyKosher.googleCallback = EventMap().googleCallback;
+  window.MostlyKosher = window.MostlyKosher || {}
+  window.MostlyKosher.googleCallback = EventMap().googleCallback;
+
+})(jQuery);
+
